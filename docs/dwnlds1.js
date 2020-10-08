beef8ed2ce7b482b82113fcc536fde0877bcbe66
Arvind Kumar <thatisarvind@gmail.com>
2020-10-08 18:58:49 +0530

    Add files via upload

---
+++ docs/dwnlds1.js
@@ -0,0 +1,20 @@
+const dwnlds1=[
+{img:"files/ciplox-en-0.webp",
+link:"files/ciplox-en.jpg",
+txt:"Ciplpx English Dose Chart"},
+
+{img:"files/ciplox-hi-0.webp",
+link:"files/ciplox-hi-0.jpg",
+txt:"Ciplpx Hindi Dose Chart"},
+
+
+];
+
+const data={
+tagLine:"One step to better health",
+
+tagDesc:"We come to you with the vision to provide short, simple and genuine videos which are prepared by health care professionals to resolve your all health concern like fitness, wellness, medication and other important beauty(skin) related issues. Hope you enjoy them and find a good solution :)",
+
+contactDesc:"Want to chat. We’d love to hear from you! Get in touch with us to inquire about hosting, domain rates, or just say hello.<br> Need help with a recent purchase? Have any questions about our websites? Drop us an email or fill out the form here.",
+
+}
